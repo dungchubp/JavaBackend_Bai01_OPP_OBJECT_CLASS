9ddd5d09e0277eeadeb671bc4099659e8e527a2c
dungchu <dungchu.bp@gmail.com>
2019-03-29 21:52:17 +0700

    Bai JavaBackend 01

---
+++ src/Bai01_OPP_OBJECT_CLASS/Bai01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai01_OPP_OBJECT_CLASS;
+
+/**
+ *
+ * @author DUNG CHU
+ */
+public class Bai01 {
+    public static void main(String[] args) {
+        
+        KhachHang kh1 = new KhachHang();
+        
+        kh1.ma = 1;
+        kh1.ten = "Dung Chu";
+        kh1.diachi = "Nghe An";
+        
+        kh1.inThongTin();
+        
+        // build javac
+        // run: java -jar
+        
+    }
+    
+}
---
+++ src/Bai01_OPP_OBJECT_CLASS/KhachHang.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai01_OPP_OBJECT_CLASS;
+
+/**
+ *
+ * @author DUNG CHU
+ */
+public class KhachHang {
+    
+    public int ma;
+    public String ten;
+    public String diachi;
+    
+    public void inThongTin()
+    {
+        System.out.println("Ma:= " + this.ma);
+        System.out.println("Ten: " + this.ten);
+        System.out.println("Dia chi: " + this.diachi);
+    }
+    
+    
+}
